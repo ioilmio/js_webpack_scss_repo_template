77e4e8b4642d2d278bb2af823f9127b5b1e42307
ioilmio <illuminato.salvatore@gmail.com>
2020-11-05 19:33:06 +0100

    clean repo

--- src/createDiv.js
+++
@@ -1,7 +0,0 @@
-const createElementDiv = function createD() {
-  const div = document.createElement('div');
-  div.textContent = 'Hello ';
-  document.body.appendChild(div);
-};
-
-createElementDiv();
--- src/source.js
+++
@@ -1,6 +0,0 @@
-import './style.css';
-import createElementDiv from './createDiv';
-
-// eslint-disable-next-line no-console
-console.log('hello pack');
-createElementDiv();
